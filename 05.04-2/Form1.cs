2e34ba873fd918c17b01797e43336fa211ba3939
Игорь Головань <golovanigorodessa@gmail.com>
2023-04-21 16:32:00 +0200

    Add project files.

---
+++ 05.04-2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05._04_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = "";
+            bool validInput = false;
+            int iterator = 0;
+            while (!validInput)
+            {
+                input = Microsoft.VisualBasic.Interaction.InputBox("Введите число:", "Запрос числа");
+                int num;
+                if (int.TryParse(input, out num))
+                {
+                    validInput = true;
+                }
+                DialogResult rez = MessageBox.Show($"Вы хотите выйти?", "Акинатор", MessageBoxButtons.YesNo);
+                if (rez != DialogResult.No)
+                {
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Введите число!");
+                    
+                    
+                }               
+            }
+            int min = 0;
+            int max = 100000000;
+            int guess = 0;
+            while (min <= max)
+            {
+                iterator++;
+                guess = (min + max) / 2;
+                DialogResult rez = MessageBox.Show($"Ваше число больше {guess}?", "Акинатор", MessageBoxButtons.YesNo);
+                if (rez == DialogResult.Yes)
+                {
+                    min = guess + 1;
+                }
+                else
+                {
+                    max = guess - 1;
+                }
+            }
+            MessageBox.Show($"Я угадал ваше число - {guess + 1}\nПонадобилось {iterator} попыток", "Акинатор");
+        }
+    }
+}
---
+++ 05.04/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05._04
+{
+    public partial class Form1 : Form
+    {
+        public static int MessageBoxCount = 0;
+        public Form1()
+        {
+            InitializeComponent();   
+            
+            MessageBoxCount++;
+            string Text1 = "Моё Резюме";
+            MessageBox.Show(Text1, "", MessageBoxButtons.OK);  
+            
+            MessageBoxCount++;
+            string Text2 = "Меня зовут - Игорь Головань.";
+            MessageBox.Show(Text2, "Имя", MessageBoxButtons.OK);
+
+            MessageBoxCount++;
+            string Text3 = "C++, C#, .NET, Html, Illustrator, Photoshop, PremierePro, Blender,\nКурица Терияки и Макароны по флотски.";
+            MessageBox.Show(Text3, "Навыки", MessageBoxButtons.OK);
+
+            MessageBoxCount++;
+            string Text4 = "Владею немецким языком на уровне В1, английским языком на уровне B2 и так же польским на уровне B1, \nчто позволяет мне свободно общаться на этих языках.";
+            MessageBox.Show(Text4, "Языки", MessageBoxButtons.OK);
+
+            MessageBoxCount++;          
+            string Text5 = "Законченное полное среднее образование - Гимназия №5.\n Незаконченное полное высшее образование - ОТУШ\"ШАГ\".";
+            MessageBox.Show(Text5, "Образование", MessageBoxButtons.OK);
+
+            MessageBoxCount++;
+            string ResultText = Text1 + Text2 + Text3 + Text4 + Text5;
+            double ResultCount = ResultText.Length / MessageBoxCount;
+            MessageBox.Show($"{ResultCount}","Среднее число символов");
+        }
+
+      
+    }
+}
